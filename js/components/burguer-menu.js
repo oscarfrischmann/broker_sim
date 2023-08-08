7fc7b8705f65e0c736db42c200ffdc95860550fb
Oscar Frischmann <oscarfrisc@gmail.com>
2023-08-08 15:29:32 -0300

    close navMenu finished

--- js/components/burguer-menu.js
+++ js/components/burguer-menu.js
@@ -1,13 +1,18 @@
 const menuIcon = document.getElementById("burguerIcon");
 const navMenu = document.getElementById("navMenu");
-const body = document.getElementById('main');
+const main = document.getElementById('main');
 
 menuIcon.addEventListener("click", () => {
   
 	navMenu.classList.toggle("display-none");
 });
 
-body.addEventListener('click', () => {
-    navMenu.classList.add("display-none")
-});
+// main.addEventListener('click', () => {
+//     navMenu.classList.add("display-none")
+// });
 
+window.addEventListener('click', (e)=>{
+    if (!navMenu.contains(e.target) && (!menuIcon.contains(e.target))){
+        navMenu.classList.add("display-none")
+    } 
+})
